9e35059fcca891e6b21b9920d12ec946fed2f091
guilherdrk <guibem20@gmail,com>
2024-11-04 10:24:45 -0300

    Modificando-Menu-da-Program.cs

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,8 +6,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" + "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
@@ -23,11 +22,15 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("");
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
     Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("");
+    Console.WriteLine("---------------------------------");
 
     switch (Console.ReadLine())
     {
